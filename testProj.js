415a07bf49d9551b2eefd2cf37a3078d95d38a08
ChanSingh <c.singh@Cardano.com>
2021-01-20 10:04:58 +0000

    Extending add fn

--- testProj.js
+++ testProj.js
@@ -1,6 +1,6 @@
 const variableOne = 1;
 const variableTwo = 2;
 
-const add = (number1, number2) => number1 + number2;
+const add = (number1, number2, number3) => number1 + number2 + number3;
 
 console.log(add(variableOne, variableTwo));
